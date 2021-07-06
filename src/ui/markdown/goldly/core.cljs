836efe0fba9b2801136e7759b523dbb0689cb9cd
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-07-05 22:35:31 -0500

    pinkie via deps config

--- src/ui/markdown/goldly/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns ui.markdown.goldly.core
-  (:require
-   [pinkie.pinkie :refer-macros [register-component]]
-   [ui.markdown.viewer :refer [markdown-viewer]]
-   [ui.markdown.goldly.prosemirror-atom :refer [prosemirror-atom]]))
-
-(register-component :p/markdown markdown-viewer)
-(register-component :p/prosemirror prosemirror-atom)
-
